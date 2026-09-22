6a8e2ba6280afab636c1bf274944ae21cea49751
Akriti Chauhan <chauhanakriti66@gmail.com>
2026-09-22 13:44:22 +0530

    Added some chabges for better user usge

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ export default function App() {
     <>
       <header className="app-header">
         <h1>Kanban Board</h1>
+        <p>Organize your work, track progress, and get things done.</p>
       </header>
       <Board />
     </>
